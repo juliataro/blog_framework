1d43b3176244b63b06ff8cd647c844fcd41644bf
Julia <julia.taro@khk.ee>
2021-02-20 12:26:59 +0200

    Loodud projekti andmebaasi kasutajate tabel

--- app/docs/db.sql
+++ app/docs/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Loomise aeg: Veebr 01, 2021 kell 11:09 PL
+-- Loomise aeg: Veebr 20, 2021 kell 11:18 EL
 -- Serveri versioon: 10.4.16-MariaDB
 -- PHP versioon: 7.4.12
 
@@ -15,7 +15,7 @@ SET time_zone = "+00:00";
 --
 -- Andmebaas: `blog_framework`
 --
-CREATE DATABASE IF NOT EXISTS `blog_framework` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `blog_framework` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `blog_framework`;
 
 -- --------------------------------------------------------
@@ -25,13 +25,32 @@ USE `blog_framework`;
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created` date NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `created` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indeksid tõmmistatud tabelitele
+--
+
+--
+-- Indeksid tabelile `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT tõmmistatud tabelitele
+--
+
+--
+-- AUTO_INCREMENT tabelile `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
