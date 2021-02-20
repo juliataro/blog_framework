48a0e73b69dc21f41326185b63a2feed0b697185
Julia <julia.taro@khk.ee>
2021-02-21 01:57:54 +0200

    Loodud suubamise abifunktsioon

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -52,7 +52,7 @@ class Users extends Controller
                 $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
                 if ($this->usersModel->register($data)) {
                     message('register_success', 'You are registerered and now can log in');
-                    header('Location: ' . URLROOT . '/' . 'users/login');
+                    redirect(users/login);
                 } else {
                     die('Something went wrong');
                 }
@@ -99,7 +99,7 @@ class Users extends Controller
                 $loggedInUser = $this->usersModel->login($data['email'], $data['password']);
                 if($loggedInUser){
                     $this->createUserSession($loggedInUser);
-                    header('Location: '.URLROOT.'/'.'pages/index');
+                    redirect('pages/index');
                 }else{
                     $date['password_err'] = 'Password is incorrect';
                     $this->view('users/login', $data);
@@ -121,7 +121,7 @@ class Users extends Controller
     public function logout(){
         session_unset();
         session_destroy();
-        header('Location: '.URLROOT.'/'.'users/login');
+        redirect('users/login');
     }
 
     public function createUserSession($user){
